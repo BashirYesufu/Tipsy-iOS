e70a2c2c0cc64c61fcc30063b7b22f0858e9cae8
Bashir Yesufu <yesufubashir@gmail.com>
2022-01-03 10:53:42 +0100

    Calculated the tip for a number of people

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
+    
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPercentButton: UIButton!
     @IBOutlet weak var tenPercentButton: UIButton!
     @IBOutlet weak var twentyPercentButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-    
+    var tip = 0.00
+    var numberOfPeople = 0
     
     @IBAction func tipChanged(_ sender: UIButton) {
+        
+        billTextField.endEditing(true)
+        zeroPercentButton.isSelected = false
+        tenPercentButton.isSelected = false
+        twentyPercentButton.isSelected = false
+        sender.isSelected = true
+        
+        let buttonTitle = sender.currentTitle!
+        let buttonTitleMinusPercentSign =  String(buttonTitle.dropLast())
+        let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
+        tip = buttonTitleAsANumber / 100
     }
     
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        numberOfPeople = Int(sender.value)
+    }
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+    }
 }
 
